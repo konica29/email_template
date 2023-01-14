74c7831f3a375200372c799ad9968884c2494608
konica29 <43178806+konica29@users.noreply.github.com>
2023-01-14 23:38:40 +0530

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-        Developed By Satyam Suri
+        Developed By Konica Arora
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; Lifetime All rights reserved.
